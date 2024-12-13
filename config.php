202216d230d0be351d19d20500338ad4a934a8e5
rakesh824203 <kumarrakesh41370@gmail.com>
2024-12-13 22:38:18 +0530

    "Assesement"

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = 'localhost';
+$db = 'rakesh';
+$user = 'root';
+$pass = '';
+
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-
-$host = 'localhost';
-$db = 'rakesh';
-$user = 'root';
-$pass = '';
-
-$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+include('config.php');
 
 $url = 'https://dummyjson.com/products';
 
